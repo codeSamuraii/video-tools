a35dff7dc6df7ac7cb3ab77e33f864b1fc90a926
codesamuraii <remi@heneault.fr>
2022-04-02 07:30:10 +0200

    ...

--- videotools/api/videotools.py
+++ videotools/api/videotools.py
@@ -1,22 +1,34 @@
 import ffmpeg
 from pathlib import Path
-from typing import TypeVar, Optional
 
-Source = TypeVar('Source', str, Path)
-Dest = Optional[Source]
+from .types import Dest, Source
 
 
 class VideoTools:
 
-    def __init__(self, source_video: Source, dest_video: Dest):
+    def __init__(self, source_video: Source, dest_video: Dest = ''):
         self.source = self.process_source(source_video)
-        if self.dest_video is None:
-            print(ffmpeg)
-            print('Dest none')
+        self.dest = self.process_dest(dest_video)
+
+    @staticmethod
+    def check_ffmpeg():
+        print(ffmpeg)
 
     @staticmethod
     def process_source(source: Source) -> Path:
         path = Path(source)
-        if not source.is_file():
+        if not path.is_file():
             raise FileNotFoundError
         return path
+
+    def process_dest(self, dest: Dest) -> Path:
+        path = Path(dest)
+        default_name = f'{self.source.stem}_output'
+        default_path = self.source.with_name(default_name)
+
+        if path.is_dir():
+            return path / default_name
+        if path.is_file() and not path.is_symlink():
+            return path
+
+        return default_path
